cdf8999bfae3cb1a45f710a93dd21bb7cc44026f
team5016 <huntingtonrobotics@gmail.com>
2026-02-11 17:27:52 -0500

    Scaffold subsystem classes

---
+++ src/main/java/frc/robot/subsystems/Feeder/FeederConveyor.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Feeder;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FeederConveyor extends SubsystemBase {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Feeder/Hopper.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Feeder;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Hopper extends SubsystemBase {
+    
+}
