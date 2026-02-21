75aa7959c307dc3dbccb28570d2c81e6d20c31e6
Jesse Abrahams <jesse.abrahams@gmail.com>
2026-02-21 18:39:29 -0500

    Warnings cleanup

--- src/main/java/frc/robot/subsystems/Shooter/FlywheelShooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/FlywheelShooter.java
@@ -7,9 +7,7 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.commands.WaitForSpeedCommand;
 
 public class FlywheelShooter extends SubsystemBase {
